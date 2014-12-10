15b67291d20c4c30eb1a284ab5a096c13af5ed48
John Kalantzis <john@jkal.net>
2014-12-10 16:17:11 +0200

    Fix tag editing.

--- blog/forms.py
+++ blog/forms.py
@@ -38,17 +38,22 @@ class PostForm(forms.ModelForm):
         self.request = kwargs.pop('request', None)
         super(PostForm, self).__init__(*args, **kwargs)
         self.fields['author'].widget = forms.HiddenInput()
-        self.fields['author'].label = ''
-        if self.request:
-            self.fields['author'].initial = self.request.user.pk
+        self.fields['author'].initial = self.request.user.pk
+
+        # When editing a post, prepopulate the tags field with the tags
+        # assigned to the post.
+        if self.instance and self.instance.id:
+            tags = self.instance.tagobjs.all()
+            self.fields['tags'].choices = [(x.name, x.name) for x in tags]
 
     def save(self, *args, **kwargs):
         post = super(PostForm, self).save(*args, **kwargs)
 
         # Create tags if necessary and assign them to posts.
-        # TODO: Investigate if it's possible to use bulk_create
+        # TODO: Investigate if it's possible to use bulk queries
         # instead of a loop.
         taglist = self.cleaned_data['tags']
+        post.tagobjs.clear()
         for tag in taglist:
             tag_obj, _ = Tag.objects.get_or_create(name=tag)
             post.tagobjs.add(tag_obj)
